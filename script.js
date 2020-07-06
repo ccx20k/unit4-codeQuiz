60425a38fd25020c433496ba83d6c34518a204d5
boido.boido <coreycapstick72@gmail.com>
2020-07-06 19:00:02 -0400

    started to pseudo code a bit and write some logic.

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 var questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    ///etc.
-  ];
+  {
+  title: "Commonly used data types DO NOT include:",
+  choices: ["strings", "booleans", "alerts", "numbers"],
+  answer: "alerts"
+  },
+  {
+  title: "The condition in an if / else statement is enclosed within ____.",
+  choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+  answer: "parentheses"
+  },
+  {
+  title: "Which typing convention is widely accepted as the norm in the coding community?",
+  choices: ["giraffe bumps", "camel case", "lemur tail", "squirrel sprint"],
+  answer: "camel case"
+  },
+  {
+  title: "An HTML element created by JavaScript is called ____.",
+  choices: ["JSON", "DOM Object", "AJAX", "An Array"],
+  answer: "DOM Object"
+  },
+  {
+  title: "CSS file links are contained by which kind of tags in an HTML page?",
+  choices: ["<body>", "<html>", "<div>", "<head>"],
+  answer: "<head>"
+  },
+];
+
+//////////=====  QUIZ LOGIC   =====//////////
+
+
+//CONTROLING DATA
+///WHAT DATA ARE WE TRYING TO CONTROL?
+////THE DATA WITHIN THE ARRAY OF QUESTIONS IS WHAT WE ARE TRYING TO CONTROL 
+
+//CONTROLING UI
+///WHAT PARTS OF THE UI ARE WE TRYING TO CONTROL?
+////WE ARE TRYING TO CONTROL THE LIST ITEMS AND THE QUESTIONS IN UI 
+////WE ARE ALSO TRYING TO ALERT THE USER ON IF THEY ARE ANSWERING CORRECTLY OR INCORRECTLY
+
+//CONTROLLER 
