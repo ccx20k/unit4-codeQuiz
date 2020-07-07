ecbb3750a4d2faeaae70ca45f2db9d012ed1c6d9
boido.boido <coreycapstick72@gmail.com>
2020-07-06 21:46:27 -0400

    got the timer to work with the start button

--- script.js
+++ script.js
@@ -1,35 +1,55 @@
 //Global variable list 
-// var startBtn = document.getElementById("start-btn");
-
+let sec = 75
 
 //Variable containing the question array, keys and properties. 
 var questions = [
   {
-  title: "Commonly used data types DO NOT include:",
-  choices: ["strings", "booleans", "alerts", "numbers"],
-  answer: "alerts"
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
   },
   {
-  title: "The condition in an if / else statement is enclosed within ____.",
-  choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-  answer: "parentheses"
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
   },
   {
-  title: "Which typing convention is widely accepted as the norm in the coding community?",
-  choices: ["giraffe bumps", "camel case", "lemur tail", "squirrel sprint"],
-  answer: "camel case"
+    title: "Which typing convention is widely accepted as the norm in the coding community?",
+    choices: ["giraffe bumps", "camel case", "lemur tail", "squirrel sprint"],
+    answer: "camel case"
   },
   {
-  title: "An HTML element created by JavaScript is called ____.",
-  choices: ["JSON", "DOM Object", "AJAX", "An Array"],
-  answer: "DOM Object"
+    title: "An HTML element created by JavaScript is called ____.",
+    choices: ["JSON", "DOM Object", "AJAX", "An Array"],
+    answer: "DOM Object"
   },
   {
-  title: "CSS file links are contained by which kind of tags in an HTML page?",
-  choices: ["<body>", "<html>", "<div>", "<head>"],
-  answer: "<head>"
+    title: "CSS file links are contained by which kind of tags in an HTML page?",
+    choices: ["<body>", "<html>", "<div>", "<head>"],
+    answer: "<head>"
   },
 ];
+
+
+$(document).ready(function () {
+
+  //BUTTON FUNCTIONALITY
+  $("#start-btn").on("click", function () {
+    console.log("sup dawg");
+    $("#welcome-box").hide();// hides the welcome box and all of its contents
+    $("#q-box").show();// shows the question box 
+  });
+});
+
+// TIMER FUNCTIONALITY
+////LEAVE THE SUBTRACTED TIEM FOR WRONG ANSWER IN THE QUESTIONS FUNCTION!
+var timer = setInterval(function () {
+  $("#timer").text(sec--);
+  if (sec == -1) {
+    clearInterval(timer);
+    //need to link this to the end page for if the questions arent all answered.. 
+  }
+}, 1000);
 
 //////////=====  QUIZ LOGIC   =====//////////
 
@@ -43,16 +63,6 @@ var questions = [
 ////a. timer at seconds = array.length * 15000(15 seconds)
 ////b. needs to hide the start button/welcome box 
 ////c. needs to bring in the new information from the question array 
-
-$(document).ready(function(){
-  $("#start-btn").on("click", function() {
-    console.log("sup dawg");
-    $("#welcome-box").hide();// hides the welcome box and all of its contents
-    $("#q-box").show();
-  });
-});
-
-
 
 //CONTROLING UI
 ///WHAT PARTS OF THE UI ARE WE TRYING TO CONTROL?
